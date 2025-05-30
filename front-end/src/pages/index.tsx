e990b9cb7de7ca015dd3f2ed8398a8564bcc3bb2
Houesse Elom <houesselazard@gmail.com>
2025-05-30 13:41:14 -0400

    Initial commit

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,18 @@
+
+
+import { Seo } from "@/ui/components/seo";
+
+
+
+
+
+export default function Home() {
+  return(
+    <>
+    <Seo title="Coders Monkeys" description="Description....."/>
+    <h1>je suis la</h1>
+    
+    </>
+    
+  );
+}
